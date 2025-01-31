e544a0e524d316d5b0991d3489d3a129aecfdb20
ReVuz <revathymr916@gmail.com>
2025-01-31 19:41:59 +0530

    Initial changes

---
+++ sections/About.jsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import { TypingText } from '../components';
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
+const About = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0" />
+
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      <TypingText
+        title="| About Altair"
+        textStyles="text-center"
+      />
+
+      <motion.p
+        variants={fadeIn('up', 'tween', 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
+      >
+        <span className="font-extrabold"> Altair® Inspire™  </span> transforms simulation into a simple and powerful tool for design optimization. With its intuitive interface and comprehensive features, Inspire helps you push the boundaries of component design and accelerate product development from concept to reality. Whether you are aiming for performance or manufacturability, Inspire offers a seamless experience to create, optimize, and study innovative parts and assemblies.
+      </motion.p>
+
+      <motion.img
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        src="/arrow-down.svg"
+        alt="arrow-down"
+        className="w-[18px] h-[28px] object-contain mt-[28px]"
+      />
+    </motion.div>
+  </section>
+);
+
+export default About;
