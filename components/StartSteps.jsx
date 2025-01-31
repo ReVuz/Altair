406d9bf6c8cc31eca7fabd10a4af5dcb3ed47122
ReVuz <revathymr916@gmail.com>
2025-01-31 19:53:45 +0530

    corrected

--- components/StartSteps.jsx
+++ components/StartSteps.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import styles from '../styles';
 import parse from 'html-react-parser';
+import styles from '../styles';
 
 const StartSteps = ({ number, text }) => (
   <div className={`${styles.flexCenter} flex-row`}>
