2b9c1e267f4c16796d1718ffb4ee0ed2243054de
Deriverx2 <benson.e.benny@gmail.com>
2025-02-02 21:03:46 +0530

    Changed footer quote

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -19,7 +19,7 @@ const Footer = () => (
     <div className={`${styles.innerWidth} mx-auto flex flex-col gap-12 relative z-10`}>
       <div className="flex items-center justify-between flex-wrap gap-5">
         <h4 className="font-bold md:text-[64px] text-[44px] text-white">
-          Ready to Submit!
+          Register here
         </h4>
         <a href="https://docs.google.com/forms/d/e/1FAIpQLSdVKCPMU3TpIA1ZvLnPXarsxiSnkjq6fTqbaup60zy_gg4tTQ/viewform?usp=sharing"
           target="_blank"
