26e9c0ec34cf9cfa24f27fd82114f216eec2b6bc
ReVuz <revathymr916@gmail.com>
2025-02-03 20:15:41 +0530

    Removed metadata

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,5 @@
 import '../styles/globals.css';
 
-export const metadata = {
-  title: 'Altair',
-  description: 'transforms simulation into a simple and powerful tool for design optimization. With its intuitive interface and comprehensive features, Inspire helps you push the boundaries of component design and accelerate product development from concept to reality. Whether you are aiming for performance or manufacturability, Inspire offers a seamless experience to create, optimize, and study innovative parts and assemblies.',
-};
 const RootLayout = ({ children }) => (
   <html lang="en">
     <head>
