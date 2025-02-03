ecbb6759ccebc9108a7ad9b89662acd22bda397f
ReVuz <revathymr916@gmail.com>
2025-02-03 20:20:36 +0530

    Page redirect approach-3

--- app/page.js
+++ app/page.js
@@ -1,5 +1,19 @@
-import { redirect } from 'next/navigation';
+'use client';
 
-export default function Page() {
-  redirect('/Altair');
-}
+import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+
+const Page = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('/Altair');
+  }, [router]);
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-primary-black">
+      <div className="text-white text-lg">Redirecting...</div>
+    </div>
+  );
+};
+
+export default Page;
